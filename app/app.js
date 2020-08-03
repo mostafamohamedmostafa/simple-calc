8ce9a95031c803d3b58ab4e2f606f174d5c980e6
mostafamohamedmostafa <54216066+mostafamohamedmostafa@users.noreply.github.com>
2020-08-03 03:42:13 +0200

    adding add section

---
+++ app/app.js
@@ -0,0 +1,8 @@
+
+
+function plus() {
+    var input_num_1 = Number(document.getElementById('input-number-1').value);
+    var input_num_2 = Number(document.getElementById('input-number-2').value);
+    document.getElementById('result').innerHTML = input_num_1 + input_num_2;
+
+};
