d31f0e73f828896ff72caee0d76d23da247259ce
mostafamohamedmostafa <54216066+mostafamohamedmostafa@users.noreply.github.com>
2020-08-03 04:09:14 +0200

    adding remining sections

--- app/app.js
+++ app/app.js
@@ -1,8 +1,31 @@
 
-
 function plus() {
-    var input_num_1 = Number(document.getElementById('input-number-1').value);
-    var input_num_2 = Number(document.getElementById('input-number-2').value);
-    document.getElementById('result').innerHTML = input_num_1 + input_num_2;
+    var add_num_1 = Number(document.getElementById('add-number-1').value);
+    var add_num_2 = Number(document.getElementById('add-number-2').value);
+    document.getElementById('result').innerHTML = add_num_1 + add_num_2;
+};
+
+
+function subtraction() {
+    var sub_num_1 = Number(document.getElementById('sub-number-1').value);
+    var sub_num_2 = Number(document.getElementById('sub-number-2').value);
+    document.getElementById('sub-result').innerHTML = sub_num_1 - sub_num_2;
+
+};
+
+
+
+
+function divide() {
+    var divide_num_1 = Number(document.getElementById('divide-number-1').value);
+    var divide_num_2 = Number(document.getElementById('divide-number-2').value);
+    document.getElementById('divide-result').innerHTML = divide_num_1 / divide_num_2;
+
+};
+
+function multiply() {
+    var multiply_num_1 = Number(document.getElementById('multiply-number-1').value);
+    var multiply_num_2 = Number(document.getElementById('multiply-number-2').value);
+    document.getElementById('multiply-result').innerHTML = multiply_num_1 * multiply_num_2;
 
 };
